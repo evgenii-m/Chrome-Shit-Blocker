dcdc5a648963c2016c5940f0218c94d766ea1e8f
evgenii-m <mez.e.s@yandex.ru>
2017-09-16 16:01:32 +0300

    Implemented apply filters when content load by scroll

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
 
-chrome.tabs.executeScript({ file: "filterContent.js" }, function() {
-  if (chrome.runtime.lastError) {
-    console.log("ERROR: " + chrome.runtime.lastError.message);
-  } 
+function performFiltering() {
+  chrome.tabs.executeScript({ file: "filterContent.js" }, function() {
+    if (chrome.runtime.lastError) {
+      console.log("ERROR: " + chrome.runtime.lastError.message);
+    } 
+  });
+}
+
+$(document).ready(function() {
+  performFiltering();
 });
-
-
-// chrome.tabs.query({'url': 'https://www.youtube.com/*'}, function(tabs) {
-// 	for (var tab in tabs) {
-// 	  chrome.tabs.executeScript(tab.id, { file: "filterContent.js" });
-// 	}
-// });
--- filterContent.js
+++ filterContent.js
@@ -1,22 +1,38 @@
+var documentHeight;
 
-chrome.storage.sync.get({'dictionary': []}, function (result) {  
-  console.log("Start filter content!");
+$(document).ready(function() {
+  documentHeight = $(document).height();
+  applyFilter();
+}); 
 
-  var elementsTitleMap = {};
-
-  $("ytd-grid-video-renderer").each(function(i, e) {
-    var videoTitle = $("a#video-title", e).text();
-    elementsTitleMap[videoTitle] = e;
-  });
-
-  for (var keywordId in result.dictionary) {
-    var keyword = result.dictionary[keywordId];
-    for (var t in elementsTitleMap) {
-      if (t.indexOf(keyword) != -1) {
-        var element = elementsTitleMap[t];
-        element.style.display = 'none';
-        console.log(t);
-      }
-    }
+$(window).scroll(function() {
+  var currentDocumentHeight = $(document).height();
+  if (currentDocumentHeight != documentHeight) {
+    documentHeight = currentDocumentHeight;
+    applyFilter();    
   }
 });
+
+function applyFilter() {
+  chrome.storage.sync.get({'dictionary': []}, function (result) {  
+    console.log("Start filter content!");
+
+    var elementsTitleMap = {};
+
+    $("ytd-grid-video-renderer").each(function(i, e) {
+      var videoTitle = $("a#video-title", e).text();
+      elementsTitleMap[videoTitle] = e;
+    });
+
+    for (var keywordId in result.dictionary) {
+      var keyword = result.dictionary[keywordId];
+      for (var t in elementsTitleMap) {
+        if (t.indexOf(keyword) != -1) {
+          var element = elementsTitleMap[t];
+          element.style.display = 'none';
+          console.log(t);
+        }
+      }
+    }
+  });
+}
